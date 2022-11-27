6399f14466984b9348aa41773e4a68de1f0b6839
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2022-11-27 18:53:50 +0100

    instalado y configurado vant ui

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template lang="">
-  <div>
+  <van-config-provider theme="dark">
     <router-view></router-view>
-  </div>
+  </van-config-provider>
 </template>
 <script>
 export default {
-  
+
 }
 </script>
 <style lang="">
-  
+
 </style>
--- src/main.js
+++ src/main.js
@@ -7,7 +7,14 @@ import router from './router';
 import 'v-calendar/dist/style.css';
 import VCalendar from 'v-calendar';
 
+// vant-ui
+import { ConfigProvider } from 'vant';
+import { Locale } from 'vant';
+import esEs from 'vant/es/locale/lang/es-ES';
+Locale.use('es-Es', esEs);
+
 const app = createApp(App);
 app.use(router);
-app.use(VCalendar, {})
+app.use(VCalendar, {});
+app.use(ConfigProvider);
 app.mount('#app');
