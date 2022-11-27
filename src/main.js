a484fcb8088f2ecfcc585db377e05f428a429494
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2022-11-27 18:40:35 +0100

    instalado y configurado vcalendar

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 
+// v-calendar
+import 'v-calendar/dist/style.css';
+import VCalendar from 'v-calendar';
+
 const app = createApp(App);
 app.use(router);
+app.use(VCalendar, {})
 app.mount('#app');
