7a62a7595d2ef62f5341e2f7174be111fc863dfc
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2022-12-10 17:13:12 +0100

    Api service y auth service

---
+++ src/services/ApiService.js
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import _ from 'lodash'
+import router from '../router/index'
+
+const ApiService = axios.create({
+    baseURL: window.location.protocol + '//' + window.location.hostname, // import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD",
+        "Access-Control-Allow-Headers": "Content-type",
+        "cache-control": "no-cache",
+    }
+})
+
+ApiService.interceptors.request.use(
+    (config) => {
+        // config.headers.common["Content-type"] = "application/json; charset=utf-8" ;
+        config.headers.common["Access-Control-Allow-Origin"] = "*";
+        config.headers.common["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD"
+        config.headers.common["Access-Control-Allow-Headers"] = "Content-type";
+        config.headers.common["cache-control"] = "no-cache";
+        // config.headers["Content-type"] = "application/json; charset=utf-8" ;
+        // config.headers["Access-Control-Allow-Origin"] = "*";
+        // config.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD"
+        // config.headers["Access-Control-Allow-Headers"] = "Content-type";
+        // config.headers["cache-control"] = "no-cache";
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+);
+
+ApiService.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        //console.log(error);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            // console.log(error.response.data);
+            // console.log(error.response.status);
+            // console.log(error.response.headers);
+            if (error.response.status === 401 && (router.currentRoute.value.name !== 'login')) {
+                //localStorage.removeItem(import.meta.env.VITE_LOCAL_STORAGE);
+                // window.location = '/login'
+                router.push({ name: "login" });
+            }
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+        //console.log(error.config);
+
+        return Promise.reject(error)
+    });
+
+ApiService.clearCredentials = function () {
+    delete this.defaults.headers.common['Authorization'];
+};
+
+ApiService.setCredentials = function (token_type, access_token) {
+    this.defaults.headers.common['Authorization'] = token_type + ' ' + access_token;
+};
+
+
+ApiService.params = function (data) {
+    return Object.keys(data)
+        .map(key => `${key}=${encodeURIComponent(_.isObject(data[key]) ? JSON.stringify(data[key]) : data[key])}`)
+        .join('&');
+}
+
+export default ApiService;
---
+++ src/services/AuthService.js
@@ -0,0 +1,127 @@
+
+import ApiService from './ApiService';
+
+const AuthService = {
+
+    token_type: null,
+    access_token: null,
+    expires_at: null,
+    intended: null,
+
+    async login(data) {
+        this.clearParams();
+        return ApiService.post('/api/login', data)
+            .then(res => {
+                this.setParams(res.data);
+                return res;
+            })
+    },
+
+    // async refresh() {
+    //     const credentials = {
+    //         grant_type: 'refresh_token',
+    //         client_id: import.meta.env.VITE_CLIENT_ID,
+    //         client_secret: import.meta.env.VITE_CLIENT_SECRET,
+    //         refresh_token: this.refresh_token,
+    //     };
+
+    //     return ApiService.post('/oauth/token/refresh', credentials)
+    //         .then(res => {
+    //             return res;
+                
+    //         }).catch((_e) => {
+    //             this.clearParams()
+    //         });
+    // },
+
+    // async logout() {
+    //     try {
+    //         try {
+    //             return await ApiService.delete('/api/logout');
+    //         } catch (err) {
+    //             return err;
+    //         }
+    //     } finally {
+    //         this.clearParams();
+    //     }
+    // },
+
+    // setIntended(to) {
+    //     if (this.intended === null) {
+    //         this.intended = to
+    //     }
+    // },
+
+    // getIntended(clear = true) {
+    //     const result = this.intended
+    //     if (clear) {
+    //         this.clearIntended()
+    //     }
+    //     return result
+    // },
+
+    // clearIntended() {
+    //     this.intended = null
+    // },
+
+    clearParams() {
+        console.log('******* reset credentials ******************************');
+        localStorage.removeItem('token');
+        this.token_type = null;
+        this.access_token = null;
+        this.refresh_token = null;
+        this.expires_in = 0;
+        ApiService.clearCredentials();
+    },
+
+    setParams(data) {
+        this.token_type = data.type;
+        this.access_token = data.access_token;
+        this.expires_at = data.expires_at;
+        ApiService.setCredentials('Bearer', data.access_token);
+        this.storeParams();
+    },
+
+    getParams() {
+        return {
+            token_type: this.token_type,
+            access_token: this.access_token,
+            expires_at: this.expires_at,
+        }
+    },
+
+    storeParams() {
+        const params = this.getParams();
+        localStorage.setItem('token', 
+            this.crypt(JSON.stringify(params), 'token'));
+    },
+
+    // loadParams() {
+    //     const params = localStorage.getItem(
+    //         import.meta.env.VITE_LOCAL_STORAGE);
+    //     if (params !== null) {
+    //         const k = this.crypt(params,
+    //             import.meta.env.VITE_LOCAL_STORAGE)
+    //         this.setParams(JSON.parse(k));
+    //     }
+    // },
+
+    crypt(input, key) {
+        var output = [];
+        var charCode;
+        for (var i = 0; i < input.length; i++) {
+            charCode = input.charCodeAt(i) ^ key[i % key.length].charCodeAt(0);
+            output.push(String.fromCharCode(charCode));
+        }
+        return output.join("");
+    },
+
+    // loggedIn() {
+    //     if (this.access_token == null) {
+    //         this.loadParams();
+    //     }
+    //     return !!this.access_token;
+    // }
+}
+
+export default AuthService;
