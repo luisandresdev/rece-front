70b78800b72de1dea314433561eb527a8e99808b
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2022-12-10 19:20:02 +0100

    componente button submit

---
+++ src/components/buttonSubmit.vue
@@ -0,0 +1,33 @@
+<template>
+    <van-button round block :color="color" :size="size" :native-type="type" @click="onSubmit">
+        <slot></slot>
+    </van-button>
+</template>
+<script setup>
+const emit = defineEmits(['onSubmit',]);
+
+defineProps({
+    color: {
+        type: String,
+        required: false,
+        default: "#6b21a8",
+    },
+    size: {
+        type: String,
+        required: false,
+        default: "small",
+    },
+    type: {
+        type: String,
+        required: false,
+        default: "submit",
+    },
+});
+
+const onSubmit = () => {
+    emit('onSubmit');
+}
+</script>
+<style lang="">
+    
+</style>
