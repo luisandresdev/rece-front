1487042d8d8ae4dc0b66e181839edcc0b9aa975f
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2022-12-11 16:50:01 +0100

    layout para las paginas del auth

---
+++ src/pages/profile/layout.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <van-nav-bar
+      :title="title"
+      left-text="Volver"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+    <slot ></slot>
+  </template>
+<script setup>
+import { useRouter } from 'vue-router';
+//props
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+//data
+const router = useRouter();
+
+// methods
+const onClickLeft = () => {
+  router.back();
+}
+
+
+</script>
+<style lang=""></style>
+  
