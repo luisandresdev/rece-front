c7fc7b49b0967178054d61ea98a718277f4f0784
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2022-12-11 16:48:42 +0100

    containers

---
+++ src/container/actionButtons.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="action-buttons">
+        <slot name="btn-submit"></slot>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang='scss' scoped>
+.action-buttons {
+    margin: 1rem 1rem 0rem 1rem;
+}
+</style>
---
+++ src/container/formContainer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="form-container" :id="`form-${name}`">
+        <slot></slot>
+    </div>
+</template>
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        required: false,
+        default: 'form',
+    }
+})
+</script>
+<style lang='scss'>
+.form-container {
+    margin-top: 1.5rem;
+}
+</style>
