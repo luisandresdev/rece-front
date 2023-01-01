bf761260370943d97b07d93cb7f63ce0e55b44a8
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-01 19:02:00 +0100

    componente con lista swipe

---
+++ src/components/listSwipeData.vue
@@ -0,0 +1,33 @@
+<template>
+    <van-swipe-cell v-for="item in data" :key="item.id">
+        <van-cell :border="true" :title="item.name" />
+        <template #right>
+            <van-button square type="danger" text="Borrar" @click="deleteItem(item)" />
+            <van-button square type="primary" text="Editar" @click="editItem(item)" />
+        </template>
+    </van-swipe-cell>
+</template>
+<script setup>
+const props = defineProps({
+    data: {
+        type: Array,
+        required: true,
+    },
+});
+
+const emit = defineEmits(['deleteItem', 'editItem']);
+
+
+
+// métodos
+const deleteItem = (item) => {
+    emit('deleteItem', item)
+}
+const editItem = (item) => {
+    emit('editItem', item)
+}
+
+</script>
+<style lang=''>
+
+</style>
