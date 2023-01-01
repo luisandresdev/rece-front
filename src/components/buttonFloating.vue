3598ca20f7a05dc6ebe15f784c61f2daf64d3a55
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-01 19:01:05 +0100

    componente button floating

---
+++ src/components/buttonFloating.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="btn-floating">
+        <van-button round icon="plus" type="success" @click="open" />
+    </div>
+</template>
+<script setup>
+const emit = defineEmits(['open',]);
+const open = () => {
+    emit('open');
+}
+</script>
+<style lang='scss' scoped>
+.btn-floating {
+    position: fixed;
+    right: 23px;
+    bottom: 23px;
+
+}
+</style>
