80eb141f1866fba17c159271fb53d1ed7e67a318
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-15 16:18:20 +0100

    quitar cosas al tabbar

--- src/pages/tabbar/index.vue
+++ src/pages/tabbar/index.vue
@@ -1,11 +1,7 @@
 <template>
-    <van-nav-bar title="Recetas" @click-right="onClickRight">
-        <template #right>
-            <van-image round width="2.3rem" height="2.3rem"
-                src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" />
-        </template>
-    </van-nav-bar>
-    <router-view></router-view>
+    <div style="margin-top:2.8rem">
+        <router-view></router-view>
+    </div>
     <van-tabbar v-model="active" active-color="#4c1d95" inactiveColor="#A1A1AA">
         <van-tabbar-item icon="calendar-o" :to="{ name: 'recipes' }">Recetas</van-tabbar-item>
         <van-tabbar-item icon="wap-home-o" :to="{ name: 'shopping-list' }">Lista de la compra</van-tabbar-item>
@@ -14,16 +10,9 @@
 </template>
 <script setup>
 import { ref } from 'vue';
-import { usePush } from '../../composables/push';
 // data
 const active = ref(false);
 
-const { redirectPush } = usePush();
-
-//methods
-const onClickRight = () => {
-    redirectPush('profile')
-}
 </script>
 <style lang=''>
 
