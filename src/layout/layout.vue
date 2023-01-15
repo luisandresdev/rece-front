cdd2cbfcd182b2fe95bb9287d71fd2bfd5d7cb1b
Luis Andrés Bolaños Yapo <byluisandresdev@gmail.com>
2023-01-15 16:18:06 +0100

    layout global de la aplicación

--- src/layout/layout.vue
+++ src/layout/layout.vue
@@ -1,14 +1,24 @@
 <template lang="">
-    <van-nav-bar
-      :title="title"
-      left-text=""
-      left-arrow
-      @click-left="onClickLeft"
-    />
-    <slot></slot>
-  </template>
+  <van-nav-bar
+    :title="title"
+    left-text=""
+    left-arrow
+    @click-left="onClickLeft"
+    @click-right="onClickRight" 
+    :fixed="true"
+  >
+  <template #right>
+          <van-image round width="2.3rem" height="2.3rem"
+              src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" />
+      </template>
+  </van-nav-bar>
+ 
+  <slot></slot>
+</template>
 <script setup>
+import {ref} from 'vue';
 import { useRouter } from 'vue-router';
+import { usePush } from '../composables/push';
 //props
 defineProps({
   title: {
@@ -19,13 +29,15 @@ defineProps({
 
 //data
 const router = useRouter();
+const { redirectPush } = usePush();
 
 // methods
 const onClickLeft = () => {
   router.back();
 }
-
+const onClickRight = () => {
+    redirectPush('profile')
+}
 
 </script>
 <style lang=""></style>
-  
